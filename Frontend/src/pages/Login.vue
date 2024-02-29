0545ecfe6ae50bb2bb268371f14e3aeb1965ac08
marusegoviac <mrsegovia6@gmail.com>
2024-02-28 23:36:59 -0400

    added logic to handle session on app.vue + validated login and register forms + connected forms to API

--- Frontend/src/pages/Login.vue
+++ Frontend/src/pages/Login.vue
@@ -12,11 +12,12 @@
       <h2 class="text-dark text-center text-2xl lg:mt-14">Se connecter</h2>
 
       <div class="mt-20 sm:mx-auto sm:w-full sm:max-w-sm">
-        <form class="space-y-6" action="#">
+        <form class="space-y-6" action="#" method="POST">
           <div>
             <label for="name" class="block text-sm text-dark">E-mail</label>
             <div class="mt-2">
               <input
+                v-model.trim="dataForm.email"
                 id="email"
                 name="email"
                 type="email"
@@ -34,6 +35,7 @@
             >
             <div class="mt-2">
               <input
+                v-model.trim="dataForm.password"
                 id="password"
                 name="password"
                 type="password"
@@ -45,12 +47,18 @@
             </div>
           </div>
 
+          <p v-if="textError" class="mt-2 text-sm text-medium-red">
+            {{ textError }}
+          </p>
+
           <div>
             <button
               type="submit"
+              :disabled="isLoading"
               class="flex w-full mt-14 justify-center rounded-md bg-primary p-3 text-md text-dark"
+              @click.prevent="onSignIn()"
             >
-              Suivant
+              {{ isLoading ? "Chargement ..." : "Suivant" }}
             </button>
           </div>
 
@@ -72,12 +80,70 @@
 
 <script>
 import { useRouter } from "vue-router";
+import { inject, ref } from "vue";
+import axios from "axios";
 
 export default {
   setup() {
     const router = useRouter();
 
+    // Inject
+    const sessionValue = inject("session");
+
+    const isLoading = ref(false);
+    const dataForm = ref({
+      email: "",
+      password: "",
+    });
+    const textError = ref("");
+
+    // * Sign in user *
+    const onSignIn = async () => {
+      const { email, password } = dataForm.value;
+      const regExEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+      if (!email || !password) {
+        return (textError.value = "Tous les champs sont obligatoires pour vous connecter");
+      }
+      if (password.length < 8) {
+        return (textError.value = "Veuillez entrer un mot de passe fort");
+      }
+      if (!regExEmail.test(email)) {
+        return (textError.value = "Veuillez entrer un email valide");
+      }
+
+      isLoading.value = true;
+      try {
+        const { data } = await axios.post(
+          `${import.meta.env.VITE_API_URL}/login`,
+          {
+            email,
+            password,
+          }
+        );
+        if (!data.accessToken)
+          return (textError.value =
+            "Une erreur s'est produite lors de la connexion, réessayez plus tard.");
+
+        // Update session value and redirect to dashboard
+        localStorage.setItem("jwt", data.accessToken);
+        isLoading.value = false;
+        sessionValue.active();
+      } catch (error) {
+        textError.value =
+          "Nom d'utilisateur ou mot de passe incorrect, vérifiez vos données et réessayez";
+        isLoading.value = false;
+      }
+    };
+
     return {
+      // Data
+      dataForm,
+      isLoading,
+      textError,
+      // Functions
+      onSignIn,
+      // Utils
       router,
     };
   },
